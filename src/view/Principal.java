129db13471083930351fc8b6842a9601944cdfc9
Ronan Jorge <Ronan Jorge@DESKTOP-4U19BKL>
2023-03-21 01:43:38 -0300

    First Commit

---
+++ src/view/Principal.java
@@ -0,0 +1,16 @@
+package view;
+
+import br.edu.fateczl.calc.Ordenacao;
+
+public class Principal {
+	public static void main(String[] args) {
+		int[] vetor = {69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1};
+		Ordenacao ordenacao = new Ordenacao();
+		ordenacao.quicksort(vetor, 0, vetor.length - 1);
+		
+		for(int valor: vetor) {
+			System.out.print(valor + " ");
+		}
+	}
+	
+}
